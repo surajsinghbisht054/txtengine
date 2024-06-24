de7739447e22b43ac8800e046945f1b04a6759eb
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2024-06-24 12:55:08 +0530

    2.8 Seconds

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 
-from txtengine import search_lines
+#from txtengine import search_lines
 from datetime import datetime
+from typing import List
+
+def search_lines(filepath:str, search_list:List[str]):
+    with open(filepath, 'r') as fp:
+        for line in fp.readlines():
+            if all(substr in line for substr in search_list):
+                yield line
+
 filename = '2024-05-23.log'
 substr = 'a04e7103fcc14184a0f19f7ab02f54da'
-
+guid_tracker = "[guid_tracker]"
 st_time = datetime.now()
-list(search_lines(filename.encode('utf-8'), [substr.encode('utf-8')] ))
+# 2.8
+list(search_lines(filename, [substr]))
 ed_time = datetime.now()
 print(ed_time-st_time)
